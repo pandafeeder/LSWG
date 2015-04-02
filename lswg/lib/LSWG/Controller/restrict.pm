3169bd612e1e3d99e9eb8029331291a0090619ef
PandaFeeder <pandafeeder@gmail.com>
2015-04-02 21:13:29 +0800

    delete files

--- lswg/lib/LSWG/Controller/restrict.pm
+++
@@ -1,9 +0,0 @@
-package LSWG::Controller::Restrict;
-use Mojo::Base 'Mojolicious::Controller';
-
-sub hello {
-    my $self = shift;
-    $self->render(text => "Hello");
-}
-
-1;
--- lswg/lib/LSWG/Model/Schema.pm
+++
@@ -1,4 +0,0 @@
-package LSWG::Model::Schema
-use base 'DBIx::Class::Schma';
-
-__PACKAGE__->load_namespaces();
