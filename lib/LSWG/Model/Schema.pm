4d3323f370a02f542ab96fb4630b0669981b8f12
PandaFeeder <pandafeeder@gmail.com>
2015-04-03 01:00:06 +0800

    Update Schema

--- lib/LSWG/Model/Schema.pm
+++ lib/LSWG/Model/Schema.pm
@@ -1,4 +1,6 @@
-package LSWG::Model::Schema
-use base 'DBIx::Class::Schma';
+use utf8;
+package LSWG::Model::Schema;
+use base qw/DBIx::Class::Schema/;
 
 __PACKAGE__->load_namespaces();
+1;
--- lib/LSWG/Model/Schema/Result/Post.pm
+++ lib/LSWG/Model/Schema/Result/Post.pm
@@ -1,27 +1,29 @@
 package LSWG::Model::Schema::Result::Post;
-use base 'DBIx::Class::Core';
+use base qw/DBIx::Class::Core/;
 
+__PACKAGE__->load_components(qw/InflateColumn::DateTime/);
 __PACKAGE__->table('posts');
 
 __PACKAGE__->add_columns(
     
     id => {
-	data_type => 'intger',
+	data_type => 'integer',
 	is_auto_increment => 1,
     },
     author => {
 	data_type => 'text',
     },
     title => {
-	date_type => 'text',
+	data_type => 'text',
     },
     content => {
-	date_type => 'text',
+	data_type => 'text',
     },
     date_published => {
 	data_type => 'datetime',
-    }
+    },
 );
 
 
 __PACKAGE__->set_primary_key('id');
+
