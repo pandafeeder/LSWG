9692515e738c1581ff2bbfa8bcd25339f30dc4e9
PandaFeeder <pandafeeder@gmail.com>
2015-04-02 20:56:46 +0800

    update

--- lib/LSWG/Controller/index.pm
+++ lib/LSWG/Controller/index.pm
@@ -4,9 +4,11 @@ use Mojo::Base 'Mojolicious::Controller';
 # This action will render a template
 sub welcome {
   my $self = shift;
-
+  my @title = qw(post1 post2 post3);
+  my @content = qw(content1 content2 content3);
   # Render template "example/welcome.html.ep" with message
-  $self->render(title => 'post1', content => "This is content of post1");
+  #$self->render( title => $title[0], content => $content[0]);
+  #$self->render( ppp => [qw(post1 post2 post3)]);
 }
 
 1;
