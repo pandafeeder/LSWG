22246dc801882bc31bb3572ebdd53055ec6112d1
PandaFeeder <pandafeeder@gmail.com>
2015-05-12 00:13:08 +0800

    bug fixed

--- lib/LSWG/Controller/edit.pm
+++ lib/LSWG/Controller/edit.pm
@@ -1,4 +1,5 @@
 package LSWG::Controller::Edit;
+use DateTime;
 use utf8;
 use Mojo::Base 'Mojolicious::Controller';
 
@@ -21,21 +22,63 @@ sub edit_page {
 sub edit_post {
   my $self = shift;
   my $url = $self->url_for;
-  my $posts;
-  if ($url =~ /post/) {
-    $posts = $self->db->resultset('Post');
-  }elsif($url =~ /save/) {
-    $posts = $self->savedb->resultset('Save');
-  } else {
-    $self->render(text => "wrong path");
+  my $button = $self->param('p_button')|$self->param('s_button');
+  if ($button eq 'Publish' && $url =~ /post/ ) {
+    my $posts = $self->db->resultset('Post');
+    $self->update($posts);
+    $self->redirect_to('restrict');
+    return;
   }
-  my $single = $posts->search({ id => $self->stash('id')})->single;
-  my $title = $self->param('title');
-  my $content = $self->param('content');
-  $single->title($title);
-  $single->content($content);
-  $single->update;
-  $self->redirect_to('restrict');
+  if ($button eq 'Save' && $url =~ /save/) {
+    my $saves = $self->savedb->resultset('Save');
+    $self->update($saves);
+    $self->redirect_to('restrict');
+    return;
+  } 
+  if ($button eq 'Publish' && $url =~ /save/) {
+    my $posts = $self->db->resultset('Post');
+    $self->create($posts, 'post');
+    $self->redirect_to('restrict');
+    return;
+  }
+  if ($button eq 'Save' && $url =~ /post/) {
+    my $saves = $self->savedb->resultset('Save');
+    $self->create($saves, 'save');
+    $self->redirect_to('restrict');
+    return;
+  }
+}
+
+sub update {
+    my ($self, $db) = @_;
+    my $single = $db->search({ id => $self->stash('id')})->single;
+    my $title = $self->param('title');
+    my $content = $self->param('content');
+    $single->title($title);
+    $single->content($content);
+    $single->update;
+}
+
+sub create {
+    my ($self, $db, $flag) = @_;
+    if ($flag eq 'post') {
+	$db->create({
+	    title => $self->param('title'),
+	    content => $self->param('content'),
+	    author => 'Username',
+	    date_published => DateTime->now,
+        });
+        return;
+    }
+    if ($flag eq 'save') {
+	$db->create({
+	    title => $self->param('title'),
+	    content => $self->param('content'),
+	    author => 'Username',
+	    date_saved => DateTime->now,
+        });
+        return;
+    }
 }
 
 1;
